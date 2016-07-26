6c20a1ca12f5c4bd9d22eba652c86ea8d3ebed05
Dmitriy Sorochenkov <podotkos@gmail.com>
2016-07-26 16:36:09 +0300

    Update app urls

--- web/func_test/funcapp/urls.py
+++ web/func_test/funcapp/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^adddataset/$', views.add_dataset, name='adddataset'),
     url(r'^calculateall/$', views.calculate_all, name='calculateall'),
+    url(r'^status/$', views.status, name='status'),
+    url(r'^results/$', views.results, name='results'),
+    url(r'^errors/$', views.errors, name='errors'),
 ]
