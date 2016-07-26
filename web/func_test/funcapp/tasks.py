7b1b80ebfe6ed0c1ccb5500db9e22f4104182657
Dmitriy Sorochenkov <podotkos@gmail.com>
2016-07-26 13:53:44 +0300

    Handle execptions correctly

--- web/func_test/funcapp/tasks.py
+++ web/func_test/funcapp/tasks.py
@@ -2,7 +2,6 @@ import json
 
 from celery import chord
 from celery import shared_task
-from celery.result import allow_join_result
 from celery.utils.log import get_task_logger
 
 from django.db import transaction
@@ -24,39 +23,42 @@ def start_calculation(self, test_suite_id):
     with transaction.atomic():
         TestSuite.objects.filter(pk=test_suite_id).update(status=INPROGRESS)
         tests = TestRun.objects.select_related('dataset').filter(testsuite=test_suite_id, status=NEW)
-        chord(test_func.subtask((d,), link_error=log_error.s(t.id)) for t in tests for d in json.loads(t.dataset.data))(update_db.s([t.id for t in tests]))
+        chord(test_func.s(d) for t in tests for d in json.loads(t.dataset.data))(update_db.s([t.id for t in tests]))
         tests.update(status=INPROGRESS)
-
-
-@shared_task(bind=True, max_retries=0)
-def log_error(self, task_id, testrun_id):
-    logger.info('TestRun failed %s', testrun_id)
-    with allow_join_result():
-        result = self.AsyncResult(task_id)
-        result.get(propagate=False)
-        test_run = TestRun.objects.select_related('testsuite').get(pk=testrun_id)
-        test_run.status = ERROR
-        test_run.exception = result.traceback
-        test_run.testsuite.status = ERROR
-        test_run.save()
-        test_run.testsuite.save()
 
 
 @shared_task(bind=True, max_retries=0)
 def update_db(self, results, tests):
     logger.debug('tests %s, results %s', tests, results)
-    with transaction.atomic():
-        ds = [(t, d) for t in TestRun.objects.select_related('dataset').filter(pk__in=tests) for d in json.loads(t.dataset.data)]
-        for i, td in enumerate(ds):
-            t, d = td
+    failed = []
+    not_failed = []
+    ds = [(t, d) for t in TestRun.objects.select_related('dataset').filter(pk__in=tests) for d in json.loads(t.dataset.data)]
+    for i, td in enumerate(ds):
+        t, d = td
+        result = results[i]
+        if isinstance(result, Exception):
+            failed.append((t, result))
+        else:
             Result.objects.create(
                 a=d['a'],
                 b=d['b'],
-                result=results[i],
+                result=result,
                 testrun=t
             )
-        TestSuite.objects.filter(pk=t.testsuite.id).update(status=DONE)
-        TestRun.objects.filter(pk__in=tests).update(status=DONE)
+            not_failed.append(t)
+
+    TestRun.objects.filter(pk__in=[t.id for t in not_failed]).update(status=DONE)
+    for f in failed:
+        t, e = f
+        TestRun.objects.filter(pk=t.id).update(
+            status=ERROR,
+            exception=repr(e)
+        )
+
+    if failed:
+        TestSuite.objects.filter(pk=failed[0][0].testsuite.id).update(status=ERROR)
+    else:
+        TestSuite.objects.filter(pk=not_failed[0].testsuite.id).update(status=DONE)
 
 
 @shared_task(bind=True, max_retries=0)
